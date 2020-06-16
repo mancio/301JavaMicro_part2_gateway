ec8ab5c055d3325ff143ee4988e2f675a3d4b9ee
Andrea Mancini <mancioman2@gmail.com>
2020-06-16 12:11:46 +0200

    first commit hello world!

---
+++ src/main/java/com/mancio/zuulgateway/ZuulgatewayApplication.java
@@ -0,0 +1,13 @@
+package com.mancio.zuulgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuulgatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulgatewayApplication.class, args);
+	}
+
+}
