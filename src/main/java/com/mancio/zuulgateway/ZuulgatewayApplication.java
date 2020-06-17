d238e14be3b677b9a0df284afdb60d2d5c58c070
Andrea Mancini <mancioman2@gmail.com>
2020-06-17 17:10:37 +0200

    readme

--- src/main/java/com/mancio/zuulgateway/ZuulgatewayApplication.java
+++ src/main/java/com/mancio/zuulgateway/ZuulgatewayApplication.java
@@ -2,7 +2,11 @@ package com.mancio.zuulgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
+@EnableResourceServer
+@EnableZuulProxy
 @SpringBootApplication
 public class ZuulgatewayApplication {
 
--- src/test/java/com/mancio/zuulgateway/ZuulgatewayApplicationTests.java
+++ src/test/java/com/mancio/zuulgateway/ZuulgatewayApplicationTests.java
@@ -1,6 +1,6 @@
 package com.mancio.zuulgateway;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
